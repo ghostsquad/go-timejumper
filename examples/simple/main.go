bb5aacc149c59450d22f8702a915cd1114b7e6fc
Weston McNamee <ghost.squadron@gmail.com>
2019-03-25 09:05:01 -0700

    Add example

---
+++ examples/simple/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	timejumper "github.com/ghostsquad/go-timejumper"
+)
+
+func main() {
+	clock := timejumper.New()
+	t := clock.Now()
+
+	fmt.Printf("current: %v\n", t)
+
+	fmt.Println("")
+
+	fmt.Println("Dive into the future!")
+	clock.Jump(t.AddDate(0, 0, 11))
+	fmt.Printf("future: %v\n", clock.Now())
+
+	fmt.Println("")
+
+	fmt.Println("Back to the present")
+	clock.Back()
+	fmt.Printf("current: %v\n", clock.Now())
+
+	fmt.Println("")
+
+	fmt.Println("Freeze the present")
+	clock.Freeze(t)
+	fmt.Printf("frozen current: %v\n", clock.Now())
+	time.Sleep(2 * time.Second)
+	fmt.Printf("frozen current: %v\n", clock.Now())
+}
